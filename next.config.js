398360dd51f9fd07673dbf15a1ca582fd1970010
MGX <admin@mgx.dev>
2025-11-15 15:10:55 -0800

    Convertir proyecto a SSR para AWS Amplify

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Disable server-side features completely
-  experimental: {},
 };
 
-module.exports = nextConfig;
+export default nextConfig;
